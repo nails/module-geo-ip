a4be4feb2169ea7bd7074553f4482bbecbf8082b
Pablo de la Pena <p@shedcollective.org>
2014-10-02 14:28:42 +0100

    Adding construct() as child drivers may call it.

--- geo_ip/_resources/drivers/_Geo_ip_driver.php
+++ geo_ip/_resources/drivers/_Geo_ip_driver.php
@@ -10,6 +10,10 @@ class NAILS_Geo_ip_driver
 	//	Class traits
 	use NAILS_COMMON_TRAIT_ERROR_HANDLING;
 	use NAILS_COMMON_TRAIT_CACHING;
+
+	// --------------------------------------------------------------------------
+
+	public function __construct(){}
 }
 
 /* End of file _Geo_ip_driver.php */
