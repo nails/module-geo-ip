e166cb9526a6e4be55b5bbedb0a062348a561104
Pablo de la Peña <p@shedcollective.org>
2015-10-08 18:28:25 +0100

    Updated to use Namespaced Traits

--- geo_ip/_resources/drivers/_Geo_ip_driver.php
+++ geo_ip/_resources/drivers/_Geo_ip_driver.php
@@ -12,11 +12,6 @@
 
 class NAILS_Geo_ip_driver
 {
-    //  Class traits
-    use NAILS_COMMON_TRAIT_ERROR_HANDLING;
-    use NAILS_COMMON_TRAIT_CACHING;
-
-    // --------------------------------------------------------------------------
-
-    public function __construct(){}
+    use \Nails\Common\Traits\ErrorHandling;
+    use \Nails\Common\Traits\Caching;
 }
