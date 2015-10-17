9a48ad39f9811aefdade5de6e5a476faa4782da1
Pablo de la Peña <p@shedcollective.org>
2015-10-17 13:57:45 +0100

    Updated services to allow for easy app overriding

--- services/services.php
+++ services/services.php
@@ -3,12 +3,20 @@
 return array(
     'services' => array(
         'GeoIp' => function () {
-            return new \Nails\GeoIp\Library\GeoIp();
+            if (class_exists('\App\GeoIp\Library\GeoIp')) {
+                return new \App\GeoIp\Library\GeoIp();
+            } else {
+                return new \Nails\GeoIp\Library\GeoIp();
+            }
         }
     ),
     'factories' => array(
         'Ip' => function () {
-            return new \Nails\GeoIp\Result\Ip();
+            if (class_exists('\App\GeoIp\Result\Ip')) {
+                return new \App\GeoIp\Result\Ip();
+            } else {
+                return new \Nails\GeoIp\Result\Ip();
+            }
         }
     )
 );
